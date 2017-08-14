caa632e789abe32d75982bdb4decfd57540e3069
Gerald van Dalen <geraldplus@gmail.com>
2017-08-14 18:54:18 +0200

    started tkinter gui

--- main.py
+++ main.py
@@ -24,11 +24,22 @@ def downKey(event):
 
 
 # Start Tkinter app
-app = Tk()
-frame = Frame(app, width=100, height=100)
-app.bind('<Left>', leftKey)
-app.bind('<Right>', rightKey)
-app.bind('<Up>', upKey)
-app.bind('<Down>', downKey)
-frame.pack()
-app.mainloop()
+root = Tk()
+root.bind('<Left>', leftKey)
+root.bind('<Right>', rightKey)
+root.bind('<Up>', upKey)
+root.bind('<Down>', downKey)
+root.title("P2048 Game")
+
+color_alternate = True
+for i in range(3):
+    for j in range(3):
+        if color_alternate:
+            color_alternate = False
+            c = "beige"
+        else:
+            color_alternate = True
+            c = "lightgray"
+        Label(root, text="5", bg=c, width=10, height=5).grid(row=i, column = j)
+
+root.mainloop()
